7c793af5dac1a18fd7ae66f942c6216223559175
19Pawel97 <19pawel9772@gmail.com>
2021-06-26 10:21:16 +0200

    inMemoryFilmRepo implementation

--- src/main/java/filmrater/domain/DuplicatedFilmException.java
+++ src/main/java/filmrater/domain/DuplicatedFilmException.java
@@ -5,5 +5,9 @@ public class DuplicatedFilmException extends RuntimeException {
     public DuplicatedFilmException(String message, Throwable cause) {
         super(message, cause);
     }
+
+    public DuplicatedFilmException(String message) {
+        super(message);
+    }
 }
 
--- src/main/java/filmrater/infrastructure/InMemoryFilmRepository.java
+++ src/main/java/filmrater/infrastructure/InMemoryFilmRepository.java
@@ -1,11 +1,29 @@
 package filmrater.infrastructure;
 
+import filmrater.domain.DuplicatedFilmException;
 import filmrater.domain.Film;
 import filmrater.domain.FilmRepository;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class InMemoryFilmRepository implements FilmRepository {
+    private static final Map<String, Film> films = new HashMap<>();
+
     @Override
     public void saveOrThrowIfExists(Film film) {
-        throw new RuntimeException("Not implemented yet!");
+        final String key = createKey(film);
+
+        final Film theFilm = films.get(key);
+
+        if (theFilm == null) {
+            films.put(key,film);
+        } else {
+            throw new DuplicatedFilmException(film.getTitle() + " already in DB.");
+        }
+    }
+
+    private String createKey(Film film) {
+        return film.getTitle() + film.getReleaseYear();
     }
 }
